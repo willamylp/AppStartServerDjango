384b7ffb0687977fd0bc318058b0872dd4c193a3
willamylp <willamy.wlp@gmail.com>
2019-03-19 11:20:52 -0300

    Ajustes nos eventos BtnBuscar...

--- main/MainWin.py
+++ main/MainWin.py
@@ -1,8 +1,6 @@
-import os, sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QFileDialog
-
+import os
+import sys
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
 from Ui_MainWin import Ui_MainWindow
 
 class MainWindow:
@@ -11,8 +9,8 @@ class MainWindow:
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
 
-        self.appDir = self.ui.btnBuscarApp.clicked.connect(self.searchApp)
-        self.venvDir = self.ui.btnBuscarVenv.clicked.connect(self.searchVenv)
+        self.ui.btnBuscarApp.clicked.connect(self.searchApp)
+        self.ui.btnBuscarVenv.clicked.connect(self.searchVenv)
 
     def show(self):
         self.main_win.show()
@@ -20,16 +18,13 @@ class MainWindow:
     def searchApp(self):
         self.appDir = str(QFileDialog.getExistingDirectory(
             None, 'Selecionar Pasta da Aplicação', os.getenv('HOME')))
-        return self.appDir
 
     def searchVenv(self):
         self.venvDir = str(QFileDialog.getExistingDirectory(
             None, 'Selecionar Pasta da Virtualenv', os.getenv('HOME')))
-        return self.venvDir
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main_win = MainWindow()
     main_win.show()
     sys.exit(app.exec_())
-
